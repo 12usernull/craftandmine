25e023f115fc429193f0a05e65b4be818f8162b1
Ertuğrul Şakar <98587504+12usernull@users.noreply.github.com>
2025-05-25 07:45:37 +0300

    Update mainmenumanager.h

--- src/gui/mainmenumanager.h
+++ src/gui/mainmenumanager.h
@@ -1,6 +1,7 @@
 // Luanti
 // SPDX-License-Identifier: LGPL-2.1-or-later
 // Copyright (C) 2013 celeron55, Perttu Ahola <celeron55@gmail.com>
+//This is Luanti's fork no send mails Luanti's developers
 
 #pragma once
 
